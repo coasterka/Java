988fd317a81a1dde732d66a0957974c8526eb117
coasterka <tsvetanovah@gmail.com>
2014-05-15 12:14:54 +0300

    Delete PrintHometown.java

--- PrintHometown/PrintHometown.java
+++
@@ -1,7 +0,0 @@
-public class PrintHometown {
-	public static void main(String[] args) {
-		System.out.println("My hometown is Vidin");
-		int maxValue = Integer.MAX_VALUE;
-		System.out.println(maxValue);
-	}
-}
