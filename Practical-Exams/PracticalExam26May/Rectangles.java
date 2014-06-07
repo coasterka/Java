f5a3fac850d2ee353644c9dba1e17d5654fb62cb
coasterka <tsvetanovah@gmail.com>
2014-06-07 14:51:34 +0300

    Update Rectangles.java

--- Practical-Exams/PracticalExam26May/Rectangles.java
+++ Practical-Exams/PracticalExam26May/Rectangles.java
@@ -1,28 +1,21 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Rectangles {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String input = scan.nextLine();
-		Pattern pattern = Pattern.compile("\\d+");
-		Matcher matcher = pattern.matcher(input);
-		List<Integer> numbers = new ArrayList<Integer>();
-		while (matcher.find()) {
-			numbers.add(Integer.parseInt(matcher.group()));		
-		}
+		String[] rectangles = input.split("\\D+");
+		
 		int max = Integer.MIN_VALUE;
-		for (int i = 6; i < numbers.size(); i+=2) {
-			int firstA = numbers.get(i-6);
-			int firstB = numbers.get(i-5);
-			int secondA = numbers.get(i-4);
-			int secondB = numbers.get(i-3);
-			int thirdA = numbers.get(i-2);
-			int thirdB = numbers.get(i-1);
+		
+		for (int i = 6; i < rectangles.length; i+=2) {
+			int firstA = Integer.parseInt(rectangles[i-5]);
+			int firstB = Integer.parseInt(rectangles[i-4]);
+			int secondA = Integer.parseInt(rectangles[i-3]);
+			int secondB = Integer.parseInt(rectangles[i-2]);
+			int thirdA = Integer.parseInt(rectangles[i-1]);
+			int thirdB = Integer.parseInt(rectangles[i]);
 			int sum = firstA*firstB + secondA*secondB + thirdA*thirdB;
 			if (sum>max) {
 				max = sum;
@@ -30,5 +23,4 @@ public class Rectangles {
 		}
 		System.out.println(max);
 	}
-
 }
